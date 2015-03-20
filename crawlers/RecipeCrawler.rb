b83549af5e478b34181f53e1726035d45696f02c
Yoeri <yoeori@live.nl>
2015-03-20 20:30:08 +0100

    Started work on recipe crawler

---
+++ crawlers/RecipeCrawler.rb
@@ -0,0 +1,23 @@
+module Crawler
+
+	class Recipe
+
+		def initialize
+
+		end
+
+		def findIngredientByName(name)
+			return ingredient = ::Ingredient.where(name: name).first ? ingredient | false
+		end
+
+		def findIngredientById(id)
+			return ingredient = ::Ingredient.find(id) ? ingredient : false
+		end
+
+		def findIngredientByProduct(product)
+			return ingredient = product.ingredient ? ingredient : false
+		end
+
+	end
+
+end
